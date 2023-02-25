b8bc211a815e893dd2d5db1a1d2587024a04e228
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-25 10:31:16 +0100

    create api documentation

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,6 @@ from events import views
 urlpatterns = [
 
     path("", views.about, name="about"),
+    path("api/documentation/", views.api_doc, name="api_doc"),
 
 ]
--- events/views.py
+++ events/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, "events/about.html")
+
+
+def api_doc(request):
+    return render(request, "events/api_doc.html")
