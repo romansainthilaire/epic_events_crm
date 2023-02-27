a877594d2c889b71d2fa27bf6b5c491a611d3b96
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-27 13:29:54 +0100

    navbar depending on groups

--- epic_events_crm/urls.py
+++ epic_events_crm/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts import views
+
 urlpatterns = [
+    path("admin/logout/", views.log_out),
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
     path("", include("events.urls")),
