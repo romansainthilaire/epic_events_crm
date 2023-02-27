4134c16b4cc4bccf4322718c6264e1a0567e6f13
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-27 14:27:18 +0100

    refactoring

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from accounts.models import User
-from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 
 # BaseUserAdmin is used to hash passwords when creating users with the admin site
 # https://docs.djangoproject.com/en/4.1/topics/auth/customizing/
@@ -10,14 +9,10 @@ from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 
 class UserAdmin(BaseUserAdmin):
 
-    #  forms used to create and update users
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-
     list_display = ["email", "first_name", "last_name", "is_active", "is_staff", "is_admin", "team"]
     list_filter = ["groups__name", "is_active"]
     search_fields = ["first_name", "last_name", "email"]
-    ordering = ["first_name", "last_name"]
+    ordering = []
 
     # fields displayed when creating a user
     add_fieldsets = [
